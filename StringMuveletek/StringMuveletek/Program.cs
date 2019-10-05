a07e0c3f15d0259264a0f27d4d4d2fa970f530ef
Fülöp Dániel <fulopdaniel@gmail.com>
2019-10-05 12:30:58 +0200

    betű nem betű

---
+++ StringMuveletek/StringMuveletek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMuveletek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg;
+            Console.Write("Írj be valamit: ");
+            szoveg = Console.ReadLine();
+            Console.WriteLine(szoveg);
+            StringTarolo st = new StringTarolo(szoveg);
+            Console.WriteLine(st.atalakitottSzoveg());
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ StringMuveletek/StringMuveletek/StringTarolo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMuveletek
+{
+    class StringTarolo
+    {
+
+        private string inputString;
+
+        public StringTarolo(string inputString)
+        {
+            this.inputString = inputString;
+        }
+
+        public string atalakitottSzoveg() {
+
+            string atalakitottSzoveg;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsDigit(inputString[i]))
+                {
+                    sb.Append('?');
+                }
+                else
+                {
+                    sb.Append('*');
+                }
+            }
+            atalakitottSzoveg = sb.ToString();
+            return atalakitottSzoveg;
+        }
+    }
+}
